e6433bff7c6abff492d2f09f9c24e6641c3946b3
Jay Patil <jaypatil@ymail.com>
2017-09-16 16:57:29 +0530

    Book Details with Observable and Promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,21 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppService } from './app.service';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app.routing';
-import { EmployeeDetailComponent } from './emp-detail.component';
-import { EmployeeListComponent } from "./emp-list.component";
-import { EmployeeEntryComponent } from "./emp-entry.component";
-
-const COMPONENT = [AppComponent, EmployeeListComponent, EmployeeEntryComponent, EmployeeDetailComponent]
+import { ObservableComponent }  from './observable.component';
+import { PromiseComponent }  from './promise.component';
+import { BookService } from './book.service';
 
 @NgModule({
-  declarations: [ COMPONENT
-    // AppComponent,
-    // EmployeeDetailComponent,
-    // EmployeeEntryComponent,
-    // EmployeeListComponent
+  declarations: [
+    AppComponent,
+    ObservableComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+    HttpModule
   ],
-  providers: [AppService],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
